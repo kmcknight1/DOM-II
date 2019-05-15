e089912079a78c420515706fe14066e19a885dc1
kmcknight1 <katiemcknight1991@gmail.com>
2019-05-15 12:43:21 -0500

    home-page animations added

--- event-listener-game/index.js
+++ event-listener-game/index.js
@@ -1,1 +1,7 @@
-TweenMax.from(".head", 2.8, {x:35, rotation:360, opacity:0, scale:0.3, ease: Back.easeInOut.config(4), y: -200 });
+TweenMax.from(".head", 2.8, {x:35, rotation:360, opacity:0, scale:0.3, ease: Back.easeInOut.config(2), y: -200 });
+
+let startBtn = document.querySelector('.start-btn')
+
+startBtn.addEventListener('click', event => {
+    console.log("clicked");
+})
